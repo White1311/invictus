a777e6ee98b942ab3006cad948e363c8b557d9bd
White <apple@MacBook-Pro-apple-2.local>
2017-08-09 16:32:26 +0600

    feat: mobile

--- Mobile/smth.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-     if($(window).width() > 768) {
-         $("#img").attr("src", "large.png");
-     } else {
-         $("#img").attr("src", "small.png");
-     }
-});
